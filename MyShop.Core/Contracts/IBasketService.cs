d72fb5fe2f766b1785d41c40a53be2550dde725f
Gabriel Friederichs <gabriel_friederichs@yaskawa.com>
2022-05-26 14:07:03 -0500

    Finished Basket Service

---
+++ MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Core.ViewModels;
+using System.Collections.Generic;
+using System.Web;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productID);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemID);
+    }
+}
